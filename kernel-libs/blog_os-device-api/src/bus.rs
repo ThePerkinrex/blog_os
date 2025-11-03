2a5ce3e94ea9c8a236da1f3911814f28badb6b1f
ThePerkinrex <theperkinrex@gmail.com>
2025-11-03 22:38:35 +0100

    start c api

--- kernel-libs/blog_os-device-api/src/bus.rs
+++ kernel-libs/blog_os-device-api/src/bus.rs
@@ -1,4 +1,20 @@
+use core::ffi::CStr;
+
 pub trait BusDriver {
     fn bus_name(&self) -> &'static str;
     fn notice_device(&mut self, name: &str);
+}
+
+#[repr(C)]
+pub struct Bus {
+    _data: (),
+    _marker:
+        core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
+
+#[repr(C)]
+pub struct BusOps {
+    name: *const CStr,
+    data: *mut Bus,
+
 }
